dd2dc9fdd1909aa27c43967ce0ac40c00135320f
ricardovargas <mdayricardo@gmail.com>
2023-10-15 16:41:37 -0500

    ,ejoras en presentacion

--- game.js
+++ game.js
@@ -1,19 +1,20 @@
 var paper = document.querySelector('.paper');
 var rock = document.querySelector('.rock');
 var scissors = document.querySelector('.scissors');
-const elementos = ['rock', 'paper', 'scissors'];
+/* const elementos = ['rock', 'paper', 'scissors']; */
 
-
+var eleccion = document.querySelector('.resultado .desicion')
 
 var seleccion_paper = paper.addEventListener('click', () => {
-    alert('selecciono papel');
+    eleccion.textContent = 'ha elegido papel 📖';
 });
 var seleccion_rock = rock.addEventListener('click', () => {
-    alert('selecciono piedra');
+    eleccion.textContent = 'ha elegido piedra ⛰';
 });
 var seleccion_scissors = scissors.addEventListener('click', () => {
-    alert('selecciono scissors');
+    eleccion.textContent = 'ha elegido tijera ✂';
 });
+
 
 
 
