adbe58a076c912d6fabef3ecd5294c904c169bfc
ricardovargas <mdayricardo@gmail.com>
2023-10-15 17:51:08 -0500

    mejoras finales, evaluacion de resultado

--- game.js
+++ game.js
@@ -1,19 +1,41 @@
 var paper = document.querySelector('.paper');
 var rock = document.querySelector('.rock');
 var scissors = document.querySelector('.scissors');
-/* const elementos = ['rock', 'paper', 'scissors']; */
+const elementos = ['rock', 'paper', 'scissors'];
 
-var eleccion = document.querySelector('.resultado .desicion')
+
+var eleccion = document.querySelector('.resultado .desicion');
 
 var seleccion_paper = paper.addEventListener('click', () => {
-    eleccion.textContent = 'ha elegido papel 📖';
+    var eleccionUsuario = 'paper';
+    determinarResultado(eleccionUsuario);
 });
 var seleccion_rock = rock.addEventListener('click', () => {
-    eleccion.textContent = 'ha elegido piedra ⛰';
+    var eleccionUsuario = 'rock';
+    determinarResultado(eleccionUsuario);
 });
 var seleccion_scissors = scissors.addEventListener('click', () => {
-    eleccion.textContent = 'ha elegido tijera ✂';
+    var eleccionUsuario = 'scissors';
+    determinarResultado(eleccionUsuario);
 });
+
+
+function determinarResultado(eleccionUsuario) {
+    var maquinaEleccion = elementos[Math.floor(Math.random() * 2)]
+    if(eleccionUsuario === maquinaEleccion) {
+        eleccion.textContent = 'hay un empate'
+    } else if(
+      (maquinaEleccion === 'rock' && eleccionUsuario === 'paper')|| 
+      (maquinaEleccion === 'paper' && eleccionUsuario === 'scissors')|| 
+      (maquinaEleccion === 'scissors' && eleccionUsuario === 'rock') 
+      ) {
+    eleccion.textContent = 'haz ganado'}
+    else{
+   eleccion.textContent = 'haz perdido' 
+}
+};
+
+
 
 
 
