71c6b094e7d237eb648b260982806e3c750b961a
ricardovargas <mdayricardo@gmail.com>
2023-10-15 16:00:01 -0500

    cambios de estilos y fuentes

--- game.js
+++ game.js
@@ -1,3 +1,19 @@
+var paper = document.querySelector('.paper');
+var rock = document.querySelector('.rock');
+var scissors = document.querySelector('.scissors');
+const elementos = ['rock', 'paper', 'scissors'];
+
+
+
+var seleccion_paper = paper.addEventListener('click', () => {
+    alert('selecciono papel');
+});
+var seleccion_rock = rock.addEventListener('click', () => {
+    alert('selecciono piedra');
+});
+var seleccion_scissors = scissors.addEventListener('click', () => {
+    alert('selecciono scissors');
+});
 
 
 
